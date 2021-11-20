26d8bdf1a1ba57804097b2582a4b22a60812cf87
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2021-11-19 21:08:09 -0600

    Add info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const addInfo = () => {
+    Axios.post("http://localhost:3000/api/infos", {
+      name: name,
+    }).then(() => {
+      alert("Si");
+    }).catch(()=>{
+      alert("NO");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <input type="text" placeholder="Isertar aqui" 
+          onChange={(event) => {
+              setName(event.target.value);
+            }}
+        /> 
+        <button  onClick={addInfo}>Agregar</button> 
+      </div>
     </div>
   );
 }
